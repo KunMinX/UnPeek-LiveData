e851948fe7f780d49050f8f67b14c475ef5a9276
KunMinX <kunminx@gmail.com>
2020-07-04 19:25:24 +0800

    set null protect

--- event-livedata/src/main/java/com/kunminx/event/EventObserver.java
+++ event-livedata/src/main/java/com/kunminx/event/EventObserver.java
@@ -1,20 +1,25 @@
+/*
+ * Copyright 2020-present KunMinX
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.kunminx.event;
 
-import androidx.lifecycle.Observer;
-
 /**
- * Create by KunMinX at 2020/7/3
+ * Create by KunMinX at 2020/6/3
  */
-public class EventObserver<T> implements Observer<Event<T>> {
+public interface EventObserver<T> {
 
-    private Observer<T> mTObserver;
-
-    public EventObserver(Observer<T> TObserver) {
-        mTObserver = TObserver;
-    }
-
-    @Override
-    public void onChanged(Event<T> event) {
-        mTObserver.onChanged(event.getContent());
-    }
+    void onReceived(T t);
 }
