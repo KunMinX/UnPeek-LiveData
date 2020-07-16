ffcba32bfc6a65c81e91c89b331cf6afcac61971
KunMinX <kunminx@gmail.com>
2020-07-16 15:57:00 +0800

    add isAllowToClear config

--- app/src/main/java/com/kunminx/puremusic/ui/callback/SharedViewModel.java
+++ app/src/main/java/com/kunminx/puremusic/ui/callback/SharedViewModel.java
@@ -28,4 +28,7 @@ public class SharedViewModel extends ViewModel {
 
     public final UnPeekLiveData<Moment> moment = new UnPeekLiveData<>();
 
+//    public final UnPeekLiveData<Moment> test =
+//            new UnPeekLiveData.Builder<Moment>().setAllowToClear(false).create();
+
 }
