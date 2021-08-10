cd9adbdf97f56a523782f90bc1dac394a1992d48
KunMinX <kunminx@gmail.com>
2021-08-10 17:39:46 +0800

    rename UnPeekLiveData as LiveData show and supplement functions to v7 design

---
+++ unpeeklivedata/src/main/java/com/kunminx/architecture/ui/callback/MutableUnPeekLiveData.java
@@ -0,0 +1,47 @@
+package com.kunminx.architecture.ui.callback;
+
+/**
+ * TODO 唯一可信源设计
+ * 我们在 V6 中继续沿用从 V3 版延续下来的基于 "唯一可信源" 理念的设计，
+ * 来确保 "事件" 的发送权牢牢握在可信的逻辑中枢单元手里，从而确保所有订阅者收到的信息都是可靠且一致的，
+ * <p>
+ * 如果这样说还不理解，可自行查阅《LiveData 唯一可信源 读写分离设计》的解析：
+ * https://xiaozhuanlan.com/topic/2049857631
+ * <p>
+ * TODO 提供 Builder 选项的支持
+ * 我们在 V6 中继续沿用从 V3 版延续下来的 Builder 选项设计，
+ * 目前提供 "是否允许发送 null" 的选项。
+ * <p>
+ * Create by KunMinX at 2021/6/17
+ */
+public class MutableUnPeekLiveData<T> extends UnPeekLiveData<T> {
+
+  @Override
+  public void setValue(T value) {
+    super.setValue(value);
+  }
+
+  @Override
+  public void postValue(T value) {
+    super.postValue(value);
+  }
+
+  public static class Builder<T> {
+
+    /**
+     * 是否允许传入 null value
+     */
+    private boolean isAllowNullValue;
+
+    public Builder<T> setAllowNullValue(boolean allowNullValue) {
+      this.isAllowNullValue = allowNullValue;
+      return this;
+    }
+
+    public UnPeekLiveData<T> create() {
+      UnPeekLiveData<T> liveData = new UnPeekLiveData<>();
+      liveData.isAllowNullValue = this.isAllowNullValue;
+      return liveData;
+    }
+  }
+}
